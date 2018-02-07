8a7f9d04f4b420b228b9bb62d7b856c4b29b3292
Daydoosh <fumey.david@hotmail.com>
2018-02-07 22:41:09 +0100

    #4 Squelette primaire GUI
    Mise en place rudimentaire de la majorité des éléments graphiques de l'interface graphique.

---
+++ warbot-unity/Assets/ManageDragAndDrop.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageDragAndDrop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    string s = "Condition1";
+
+    // TODO
+    // Essayer de récupérer la string de l'objet sélectionné par la souris, et le passer en argument de Find("String s"), pour utiliser ce script pour tous les objets
+    public void Drag()
+    {
+        GameObject.Find(s).transform.position = Input.mousePosition;
+    }
+
+
+    // TODO
+    // Rendre la fonction plus générique, comme pour Drag(), pour gérer les différents "placeHolders"
+    public void Drop()
+    {
+        GameObject ph1 = GameObject.Find("placeHolder");
+        float distance = Vector3.Distance(GameObject.Find(s).transform.position, ph1.transform.position);
+        if (distance < 50)
+        {
+            GameObject.Find(s).transform.position = ph1.transform.position;
+        }
+    }
+}
