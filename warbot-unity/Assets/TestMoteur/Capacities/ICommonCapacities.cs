3a79cc0b9bcb9f1a87344bde491bfb6e106da344
Maxime Teixeira-Ricci <maxime.teixeira@etu.unice.fr>
2018-02-11 17:58:20 +0100

    #7 Création de l'interface des capacités
    Création des classes nécessaire à l'implementation des capacités des unités

---
+++ warbot-unity/Assets/TestMoteur/Capacities/ICommonCapacities.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICommonCapacities : AbstractCapacities
+{
+    // 
+    double GetAngle();
+    void setAngle();
+    int getID();
+	
+}
---
+++ warbot-unity/Assets/TestMoteur/Capacities/Implementation/CommonCapacities.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommonCapacities : ICommonCapacities{
+    public double GetAngle()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public int getID()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void setAngle()
+    {
+        throw new System.NotImplementedException();
+    }
+    
+}
