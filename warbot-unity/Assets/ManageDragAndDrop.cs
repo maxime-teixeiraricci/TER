401bb25c21063223977f6dd73e14a7920b29cabf
Benoit Dorey <benoitdorey@gmail.com>
2018-02-23 17:07:03 +0100

    #19 Début Drag&Drop
    Drag&Drop sans duplication d'objet et sans pièce de puzzle.

--- warbot-unity/Assets/ManageDragAndDrop.cs
+++ warbot-unity/Assets/ManageDragAndDrop.cs
@@ -4,35 +4,14 @@ using UnityEngine;
 
 public class ManageDragAndDrop : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    string s = "Condition1";
+    float distance = 10;
 
     // TODO
     // Essayer de récupérer la string de l'objet sélectionné par la souris, et le passer en argument de Find("String s"), pour utiliser ce script pour tous les objets
-    public void Drag()
+    public void OnMouseDrag()
     {
-        GameObject.Find(s).transform.position = Input.mousePosition;
-    }
-
-
-    // TODO
-    // Rendre la fonction plus générique, comme pour Drag(), pour gérer les différents "placeHolders"
-    public void Drop()
-    {
-        GameObject ph1 = GameObject.Find("placeHolder");
-        float distance = Vector3.Distance(GameObject.Find(s).transform.position, ph1.transform.position);
-        if (distance < 50)
-        {
-            GameObject.Find(s).transform.position = ph1.transform.position;
-        }
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = objPosition;
     }
 }
