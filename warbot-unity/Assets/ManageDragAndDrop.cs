54e3adebf782257d04fbb707f0cca4ab8156d9a8
Daydoosh <fumey.david@hotmail.com>
2018-02-27 22:38:54 +0100

    #21 Bases Blocs d'instructions
    Mise en place rudimentaire des blocs d'instructions, avec gestion de leur déplacement dans l'éditeur (à perfectionner)

--- warbot-unity/Assets/ManageDragAndDrop.cs
+++ warbot-unity/Assets/ManageDragAndDrop.cs
@@ -6,12 +6,73 @@ public class ManageDragAndDrop : MonoBehaviour {
 
     float distance = 10;
 
-    // TODO
-    // Essayer de récupérer la string de l'objet sélectionné par la souris, et le passer en argument de Find("String s"), pour utiliser ce script pour tous les objets
+
     public void OnMouseDrag()
     {
+        Renderer rendEditeur = GameObject.Find("Editeur").GetComponent<Renderer>();
+        Renderer rendCanvas = GameObject.Find("GameEditorScreen").GetComponent<Renderer>();
+        Renderer rendControlPanel = GameObject.Find("Panneau controle").GetComponent<Renderer>();
+        var collider2D = gameObject.GetComponent<Collider2D>();
+
+        // Dimensions du canvas
+        float widthCanvas = rendCanvas.bounds.size.x;
+        float heightCanvas = rendCanvas.bounds.size.y;
+
+
+        // Largeur et hauteur de la pièce de puzzle
+        float widthPuzzle = collider2D.bounds.size.x;
+        float heightPuzzle = collider2D.bounds.size.y;
+
+
+        // Largeur et hauteur de l'éditeur de comportement
+        float widthEditor = rendEditeur.bounds.size.x;
+        float heightEditor = rendEditeur.bounds.size.y;
+
+        // Largeur et hauteur du panneau de contrôle
+        float widthControlPanel = rendControlPanel.bounds.size.x;
+        float heightControlPanel = rendControlPanel.bounds.size.y;
+
+
         Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
         Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
         transform.position = objPosition;
+   
+        float currentPositionX = transform.position.x + widthPuzzle / 2;
+        //float maxPositionX = widthEditor - widthPuzzle;
+        float maxPositionX = widthCanvas - widthPuzzle;
+        //float minPositionX = maxPositionX - widthCanvas + widthControlPanel + (widthPuzzle / 2);
+        float minPositionX = widthControlPanel + widthPuzzle / 2;
+
+    
+
+        float currentPositionY = transform.position.y - heightPuzzle / 2;
+        float minPositionY = 0 + heightPuzzle;
+        float maxPositionY = heightEditor;
+
+
+        Debug.Log("CurrentPositionY = " + currentPositionY);
+
+        Debug.Log("POSITION Y CANVAS = " + rendCanvas.transform.position.y);
+        //Debug.Log("POSITION X EDITEUR = " + rendEditeur.transform.position.x);
+
+        if (transform.position.x > maxPositionX)
+            {
+            transform.position = new Vector3(maxPositionX, transform.position.y, transform.position.z);
+            }  
+
+        if(transform.position.x + (widthPuzzle / 2) < minPositionX)
+            {
+            transform.position = new Vector3(minPositionX - (widthPuzzle / 2), transform.position.y, transform.position.z);
+        }
+
+        if(transform.position.y < minPositionY)
+        {
+            transform.position = new Vector3(transform.position.x, minPositionY, transform.position.z);
+        }
+
+        if(transform.position.y > maxPositionY)
+        {
+            transform.position = new Vector3(transform.position.x, maxPositionY, transform.position.z);
+        }
     }
 }
---
+++ warbot-unity/Assets/Scripts/UI Scripts/GUI/createPuzzle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createPuzzle : MonoBehaviour {
+
+    public GameObject puzzle;
+    public GameObject puzzle2;
+
+    public void OnMouseDown()
+    {
+        GameObject puzzleClone = (GameObject)Instantiate(puzzle, GameObject.Find("Editeur").transform);
+    }
+}
