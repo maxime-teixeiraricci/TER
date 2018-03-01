cd90f2eba081529857ff2cc02c0bce2d17192e32
Daydoosh <fumey.david@hotmail.com>
2018-03-01 13:36:34 +0100

    #18 Bouton Undo
    Mise en place du bouton Undo

--- warbot-unity/Assets/Scripts/UI Scripts/GUI/ManageDragAndDrop.cs
+++ warbot-unity/Assets/Scripts/UI Scripts/GUI/ManageDragAndDrop.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 public class ManageDragAndDrop : MonoBehaviour {
 
     float distance = 10;
-    
+    int nbrObjects = 0;
+
+
+    public void OnMouseDown()
+    {
+        // Nombre de pièces de puzzle présentes dans l'éditeur
+        int nbrObjects = GetComponent<createPuzzle>().cptObjects;
+        Debug.Log("NOMBRE DE PIECES DE PUZZLE = " + nbrObjects);
+    }
 
     public void OnMouseDrag()
     {
@@ -79,6 +87,7 @@ public class ManageDragAndDrop : MonoBehaviour {
         Renderer rendControlPanel = GameObject.Find("Panneau controle").GetComponent<Renderer>();
         var collider2D = gameObject.GetComponent<Collider2D>();
 
+
         // Dimensions du canvas
         float widthCanvas = rendCanvas.bounds.size.x;
         float heightCanvas = rendCanvas.bounds.size.y;
@@ -108,7 +117,7 @@ public class ManageDragAndDrop : MonoBehaviour {
 
         if (transform.position.x >= minPositionX - widthPuzzle || transform.position.x <= maxPositionX)
         {
-            transform.position = new Vector3((maxPositionX / 2) +( widthPuzzle / 2), maxPositionY, transform.position.z);
+            transform.position = new Vector3((maxPositionX / 2) +( widthPuzzle / 2), maxPositionY + heightPuzzle* nbrObjects, transform.position.z);
         }
     }
 
--- warbot-unity/Assets/Scripts/UI Scripts/GUI/createPuzzle.cs
+++ warbot-unity/Assets/Scripts/UI Scripts/GUI/createPuzzle.cs
@@ -6,9 +6,27 @@ public class createPuzzle : MonoBehaviour {
 
     public GameObject puzzle;
     public GameObject puzzle2;
+    public ArrayList listPieces = new ArrayList();
+    public ArrayList recoverList = new ArrayList();
+    public int cptObjects;
+    public int sizeRecoverList;
 
     public void OnMouseDown()
     {
         GameObject puzzleClone = (GameObject)Instantiate(puzzle, GameObject.Find("Editeur").transform);
+        listPieces.Add(puzzleClone);
+        cptObjects = listPieces.Count;
+        Debug.Log("TAILLE LISTE ONCLICK DOOOOOOWN = " + cptObjects);
     }
+
+    public void Undo()
+    {
+        GameObject pieceToUndo = (GameObject)listPieces[cptObjects - 1];
+        recoverList.Add(pieceToUndo);
+        sizeRecoverList = recoverList.Count;
+        Destroy(pieceToUndo);
+        listPieces.Remove(listPieces[cptObjects - 1]);
+        cptObjects = listPieces.Count;
+    }
+
 }
