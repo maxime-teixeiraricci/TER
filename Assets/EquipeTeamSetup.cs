40886de1ac5478d55fdc941a71d0c35a253963a3
Maxime Teixeira <maxime.teixeira.93@gmail.com>
2018-03-21 12:03:00 +0100

    Ajout d'une scene menu

---
+++ Assets/EquipeTeamSetup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EquipeTeamSetup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        UpdateOptions();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+    }
+
+    public void UpdateOptions()
+    {
+        GetComponent<Dropdown>().ClearOptions();
+        GameObject.Find("GameSetting").GetComponent<GameSettingsScript>().Updating();
+        GetComponent<Dropdown>().AddOptions(GameObject.Find("GameSetting").GetComponent<GameSettingsScript>().teams);
+    }
+}
---
+++ Assets/GameSettingsScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameSettingsScript : MonoBehaviour
+{
+    public string gamePath;
+    public int nbPlayers;
+    public Dropdown nbPlayersDropdown;
+    public GameObject[] _teamsHUD;
+    public List<string> teams = new List<string>();
+
+    // Use this for initialization
+    void Start ()
+    {
+        //Updating();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        nbPlayers = int.Parse(nbPlayersDropdown.captionText.text);
+        for (int i = 0; i < _teamsHUD.Length; i ++)
+        {
+            if (i < nbPlayers) { _teamsHUD[i].SetActive(true); }
+            else { _teamsHUD[i].SetActive(false); }
+        }
+    }
+
+    string stringDifference(string s, string z)
+    {
+        string res = "";
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (i >= z.Length)
+            {
+                res = res + s[i];
+            }
+        }
+        return res;
+    }
+
+    public void Updating()
+    {
+        gamePath = "./teams/TestBot/";
+        teams = new List<string>();
+        string[] fileEntries = Directory.GetFiles(gamePath);
+        foreach (string s in fileEntries)
+        {
+            string team = stringDifference(s, gamePath);
+            if (team.Contains(".wbt"))
+            {
+                teams.Add(team);
+            }
+        }
+
+   
+    }
+}
