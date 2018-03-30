df246e218fb62bf1c30de0579bd2ef5df8165f18
Daydoosh <fumey.david@hotmail.com>
2018-03-30 15:03:44 +0200

    #31 Ebauche affichage statistiques
    A la sélection d'une unité via le dropdown, ses statistiques s'affichent dans le cadre dédié.

---
+++ Assets/Scripts/UI Scripts/GUI/StatsLoader.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEngine.UI;
+
+public class StatsLoader : MonoBehaviour
+{
+
+    public Dropdown unit;
+
+    public void ReadStats()
+    {
+        Text statText = GameObject.Find("PropertiesValues").GetComponentInChildren<Text>();
+
+        string unitName = unit.captionText.text;
+        Debug.Log("VALUE UNITNAME = " + unitName);
+        int statBase = 4;
+        int statExplo = 5;
+        int statHeavy = 6;
+        string fileName = "properties.yml";
+        TextReader reader;
+        reader = new StreamReader(fileName);
+        string line;
+        while (true)
+        {
+            // lecture de la ligne
+            line = reader.ReadLine();
+            // si la ligne est vide on arrête
+            if (line == null) break;
+
+            // Lecture des statistiques de la base
+            else if (unitName == "Base" && line.Contains("WarBase"))
+            {
+                readStatsFile(unitName, reader, statBase, statText);
+                /*
+                for(int i = 0; i < statBase; i++)
+                {
+                    line = reader.ReadLine();
+                    // on affiche la ligne
+                    //Debug.Log("VALUE LINE = " + line);
+                    if (i == 0)
+                    {
+                        string oldTexte = statText.text;
+                        //Debug.Log("VALEUR OLDTEEEEEEEEEEEEXTEEEEEE ====== " + oldTexte);
+                        //statText.text.Replace(oldTexte, line);
+                        statText.text = line;
+                    }
+                    else
+                    {
+                        string oldText = statText.text;
+                        string newText = oldText + line;
+                        Debug.Log("VALEUR OLDTEEEEEEEEEEEEXT ====== " + oldText);
+                        Debug.Log("VALEUR NEEEEEEEEEW ====== " + newText);
+                        statText.text = newText;
+                    }
+                }*/
+            }
+
+            // Lecture des statistiques des explorateurs
+
+            else if (unitName == "Explorer" && line.Contains("WarExplorer"))
+            {
+                readStatsFile(unitName, reader, statExplo, statText);
+            }
+
+            else if (unitName == "Heavy" && line.Contains("WarHeavy"))
+            {
+                readStatsFile(unitName, reader, statHeavy, statText);
+            }
+
+            else if (unitName == "Light")
+            {
+                statText.text = "No data available";
+            }
+
+
+        }
+        reader.Close();
+    }
+
+    void readStatsFile(string unitName, TextReader reader, int nbrStats, Text statText)
+    {
+        string line;
+        /*switch (unitName)
+        {
+            case "Base": nbrStats = 4;
+                break;
+            case "Explorer": nbrStats = 5;
+                break;
+            case "Heavy": nbrStats = 6;
+                break;
+        }*/
+
+        for (int i = 0; i < nbrStats; i++)
+        {
+            line = reader.ReadLine();
+            // on affiche la ligne
+            //Debug.Log("VALUE LINE = " + line);
+            if (i == 0)
+            {
+                string oldTexte = statText.text;
+                //Debug.Log("VALEUR OLDTEEEEEEEEEEEEXTEEEEEE ====== " + oldTexte);
+                //statText.text.Replace(oldTexte, line);
+                statText.text = line;
+            }
+            else
+            {
+                string oldText = statText.text;
+                string newText = oldText + line;
+                //Debug.Log("VALEUR OLDTEEEEEEEEEEEEXT ====== " + oldText);
+                //Debug.Log("VALEUR NEEEEEEEEEW ====== " + newText);
+                statText.text = newText;
+                statText.resizeTextForBestFit = true;
+            }
+        }
+
+        /*
+        else
+        {
+            statText.text = "No data available";
+        }
+        */
+    }
+
+
+}
