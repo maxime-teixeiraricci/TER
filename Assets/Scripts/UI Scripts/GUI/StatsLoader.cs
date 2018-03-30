8042014147e26a6254aa5fb63082c9d7490444d8
Daydoosh <fumey.david@hotmail.com>
2018-03-30 18:18:03 +0200

    #31 Affichage des modèles 3D
    Affichage du modèle 3D de l'untié sélectionnée

--- Assets/Scripts/UI Scripts/GUI/StatsLoader.cs
+++ Assets/Scripts/UI Scripts/GUI/StatsLoader.cs
@@ -11,9 +11,13 @@ public class StatsLoader : MonoBehaviour
     public void ReadStats()
     {
         Text statText = GameObject.Find("PropertiesValues").GetComponentInChildren<Text>();
+        Image imageUnit = GameObject.Find("ImageUnit").GetComponent<Image>();
+        Image heavySprite = GameObject.Find("HeavySprite").GetComponent<Image>();
+        Image baseSprite = GameObject.Find("BaseSprite").GetComponent<Image>();
+        Image lightSprite = GameObject.Find("LightSprite").GetComponent<Image>();
+        Image explorerSprite = GameObject.Find("ExplorerSprite").GetComponent<Image>();
 
         string unitName = unit.captionText.text;
-        Debug.Log("VALUE UNITNAME = " + unitName);
         int statBase = 4;
         int statExplo = 5;
         int statHeavy = 6;
@@ -25,6 +29,7 @@ public class StatsLoader : MonoBehaviour
         {
             // lecture de la ligne
             line = reader.ReadLine();
+
             // si la ligne est vide on arrête
             if (line == null) break;
 
@@ -32,6 +37,8 @@ public class StatsLoader : MonoBehaviour
             else if (unitName == "Base" && line.Contains("WarBase"))
             {
                 readStatsFile(unitName, reader, statBase, statText);
+                imageUnit.sprite = baseSprite.sprite;
+                imageUnit.color = new Color(imageUnit.color.r, imageUnit.color.g, imageUnit.color.b, 255);
                 /*
                 for(int i = 0; i < statBase; i++)
                 {
@@ -61,16 +68,22 @@ public class StatsLoader : MonoBehaviour
             else if (unitName == "Explorer" && line.Contains("WarExplorer"))
             {
                 readStatsFile(unitName, reader, statExplo, statText);
+                imageUnit.sprite = explorerSprite.sprite;
+                imageUnit.color = new Color(imageUnit.color.r, imageUnit.color.g, imageUnit.color.b, 255);
             }
 
             else if (unitName == "Heavy" && line.Contains("WarHeavy"))
             {
                 readStatsFile(unitName, reader, statHeavy, statText);
+                imageUnit.sprite = heavySprite.sprite;
+                imageUnit.color = new Color(imageUnit.color.r, imageUnit.color.g, imageUnit.color.b, 255);
             }
 
             else if (unitName == "Light")
             {
                 statText.text = "No data available";
+                imageUnit.sprite = lightSprite.sprite;
+                imageUnit.color = new Color(imageUnit.color.r, imageUnit.color.g, imageUnit.color.b, 255);
             }
 
 
@@ -78,49 +91,26 @@ public class StatsLoader : MonoBehaviour
         reader.Close();
     }
 
+
     void readStatsFile(string unitName, TextReader reader, int nbrStats, Text statText)
     {
         string line;
-        /*switch (unitName)
-        {
-            case "Base": nbrStats = 4;
-                break;
-            case "Explorer": nbrStats = 5;
-                break;
-            case "Heavy": nbrStats = 6;
-                break;
-        }*/
 
         for (int i = 0; i < nbrStats; i++)
         {
             line = reader.ReadLine();
-            // on affiche la ligne
-            //Debug.Log("VALUE LINE = " + line);
             if (i == 0)
             {
                 string oldTexte = statText.text;
-                //Debug.Log("VALEUR OLDTEEEEEEEEEEEEXTEEEEEE ====== " + oldTexte);
-                //statText.text.Replace(oldTexte, line);
                 statText.text = line;
             }
             else
             {
                 string oldText = statText.text;
                 string newText = oldText + line;
-                //Debug.Log("VALEUR OLDTEEEEEEEEEEEEXT ====== " + oldText);
-                //Debug.Log("VALEUR NEEEEEEEEEW ====== " + newText);
                 statText.text = newText;
                 statText.resizeTextForBestFit = true;
             }
         }
-
-        /*
-        else
-        {
-            statText.text = "No data available";
-        }
-        */
     }
-
-
 }
