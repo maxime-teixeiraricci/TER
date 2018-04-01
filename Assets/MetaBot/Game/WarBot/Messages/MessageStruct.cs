3a5deaf7e5fbe8817e311190936355092bcb6b10
Mehdi Benlamine <mehdi.ben-lamine@etu.unice.fr>
2018-04-01 19:29:30 +0200

    Ajout de MessageStruct, structure de liaison entre l'interpréteur et le moteur pour la gestion des messages.

---
+++ Assets/MetaBot/Game/WarBot/Messages/MessageStruct.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageStruct {
+
+    public string _intitule;
+    public string _destinataire;
+
+    public MessageStruct(string intitule, string destinataire)
+    {
+        _intitule = intitule;
+        _destinataire = destinataire;
+    }
+}
