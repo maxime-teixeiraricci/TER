5a7adfb03540825f43023b8de7c1d1089809972a
Mehdi Benlamine <mehdi.ben-lamine@etu.unice.fr>
2018-04-02 17:52:42 +0200

    Création de la notion de contrat et du contrat d'élimination

---
+++ Assets/MetaBot/Game/WarBot/Contract/EliminationContract.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EliminationContract : Contract
+{
+    public GameObject _target;
+
+    public EliminationContract(GameObject target)
+    {
+        _target = target;
+    }
+
+    public override bool isDone()
+    {
+        GameObject[] listeUnits = GameObject.FindGameObjectsWithTag("Unit");
+        foreach(GameObject unit in listeUnits){
+            if (unit.Equals(_target))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
