553d83c5acb7242ee7b3092d50f0424706fa9cdd
Maxime Teixeira <maxime.teixeira.93@gmail.com>
2018-04-03 17:04:48 +0200

    Modification Elimination Contract

--- Assets/MetaBot/Game/WarBot/Contract/Contract.cs
+++ Assets/MetaBot/Game/WarBot/Contract/Contract.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public abstract class Contract : MonoBehaviour {
 
+    public delegate bool Listener();
+    public Dictionary<string, Listener> _percepts = new Dictionary<string, Listener>();
+
     public abstract bool isDone();
+    public virtual void InitPercept()
+    {
+
+    }
 
 }
--- Assets/MetaBot/Game/WarBot/Contract/EliminationContract.cs
+++ Assets/MetaBot/Game/WarBot/Contract/EliminationContract.cs
@@ -23,5 +23,44 @@ public class EliminationContract : Contract
         return true;
     }
 
+    public override void InitPercept()
+    {
+        base.InitPercept();
+        _percepts["CONTRACT_ELIMINATION"] = delegate () { return GetComponent<Stats>()._contract.GetType() == gameObject.GetType(); };
+        _percepts["CONTRACT_ELIMINATION_TARGET_NEAR"] = delegate () {
+            Brain brain = GetComponent<Brain>();
+            Sight sight = brain.GetComponent<Sight>();
+            List<GameObject> _listOfUnitColl = new List<GameObject>();
+            foreach (GameObject gO in sight._listOfCollision)
+            {
+                EliminationContract contract = (EliminationContract)GetComponent<Stats>()._contract;
+                if (gO && contract._target == gO)
+                {
+                    if (gO.GetComponent<Stats>() && gO.GetComponent<Stats>()._teamIndex != GetComponent<Stats>()._teamIndex)
+                    {
+                        GetComponent<Stats>()._target = gO;
+                        GetComponent<Stats>()._heading = getAngle(gO);
+                        return true;
+                    }
+                }
+            }
+            return false;
+        };
+    }
 
+
+
+
+    public int getAngle(GameObject _gameObject)
+    {
+        Vector3 vect = _gameObject.transform.position - transform.position;
+        Vector3 projVect = Vector3.ProjectOnPlane(vect, Vector3.up);
+
+        if (projVect.z > 0)
+        {
+            return (int)(360 - Vector3.Angle(projVect, new Vector3(1, 0, 0)));
+        }
+        return (int)(Vector3.Angle(projVect, new Vector3(1, 0, 0)));
+
+    }
 }
