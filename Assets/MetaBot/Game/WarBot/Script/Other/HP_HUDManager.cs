4ae084e3914b005749f5c887a88fa9741b2ede36
Maxime Teixeira <maxime.teixeira.93@gmail.com>
2018-04-05 21:54:53 +0200

    Correction de bugs + Ajouts de differentes couleurs pour les barres de vies.

--- Assets/MetaBot/Game/WarBot/Script/Other/HP_HUDManager.cs
+++ Assets/MetaBot/Game/WarBot/Script/Other/HP_HUDManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class HP_HUDManager : MonoBehaviour
 {
     public GameObject _target;
+
     public int _value;
     public int _maxValue;
     public Text _HPText;
@@ -14,6 +15,8 @@ public class HP_HUDManager : MonoBehaviour
     public Text _ressourceText;
     public Image _ressourceImage;
     public Objet ressource;
+
+    public HPColor[] _hpColor;
 	
 	// Update is called once per frame
 	void Update ()
@@ -28,7 +31,10 @@ public class HP_HUDManager : MonoBehaviour
             _maxValue = _target.gameObject.GetComponent<Stats>()._maxHealth;
             _value = Mathf.Max(0, Mathf.Min(_maxValue, _value));
             _HPImage.fillAmount = (1.0f * _value) / _maxValue;
+            _HPImage.color = HPColorUpdate(_HPImage.fillAmount);
+
             _HPText.text = "" + _value;
+            
             transform.position = Camera.main.WorldToScreenPoint(_target.transform.position );
             transform.position += _delta;
             if (_target.GetComponent<Inventory>()._objets.ContainsKey(ressource) && _target.GetComponent<Inventory>()._objets[ressource] > 0 )
@@ -51,4 +57,25 @@ public class HP_HUDManager : MonoBehaviour
 
         
 	}
+
+
+    public Color HPColorUpdate(float HPpercent)
+    {
+        Color color = _hpColor[0].color;
+        for (int i = 1; i < _hpColor.Length; i++)
+        {
+            if (_hpColor[i - 1].hpPercent < HPpercent && HPpercent <= _hpColor[i].hpPercent) { return _hpColor[i].color; }
+        }
+        return color;
+    }
+
+
+}
+
+[System.Serializable]
+public struct HPColor
+{
+    [Range(0f,1f)]
+    public float hpPercent;
+    public Color color;
 }
