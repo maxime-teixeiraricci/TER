da8f578aec49f7ab0d0767968fbc2ad3fd847e2e
Fireez <benjaminfireez@yahoo.fr>
2018-04-06 18:04:37 +0200

    Traducteur fix

--- Assets/MetaBot/Game/WarBot/Interpreter/LangageLoader.cs
+++ Assets/MetaBot/Game/WarBot/Interpreter/LangageLoader.cs
@@ -3,54 +3,74 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
-public class LangageLoader {
-    public string Language;
+public class LangageLoader : MonoBehaviour {
+    public List<Langage> langues;
 
 
-	public LangageLoader(string l){
-        Language = l;
-	}
-
-    public bool checkFile()
+    public bool checkFile(string Language)
     {
-        bool flag = false;
-
         if (!Directory.Exists(Constants.langDirectory))
         {
             Directory.CreateDirectory(Constants.langDirectory);
-            System.Console.WriteLine("Dossier existe pas");
+           print("Dossier existe pas");
         }
 
         foreach (string file in Directory.GetFiles(Constants.langDirectory))
         {
-            System.Console.WriteLine("File : " + file);
+           print("File : " + file);
             if (file.Contains(Language))
             {
-                flag = true;
-                break;
+                return true;
             }
         }
-        System.Console.WriteLine(" flag = "+flag);
-        return flag;
+        return false;
     }
 
-    public Dictionary<string,string> readFile()
+    public Langage readFile(string Language)
     {
+        Langage l_lang = new Langage();
+        l_lang.langue = Language;
+        l_lang.trads = new List<Traduction>();
         Dictionary<string, string> result = new Dictionary<string, string>();
 
-        if (!checkFile())
-            return null;
+        if (!checkFile(Language))
+            return new Langage();
 
         string[] lines = System.IO.File.ReadAllLines(Constants.langDirectory + Language + ".txt");
         foreach (string line in lines)
         {
-            if (!(line[0] == '#'))
+            if (!(line.Contains("#")))
             {
                 string [] tmp = line.Split(':');
-                result.Add(tmp[0], tmp[1]);
+                Traduction l_tmp = new Traduction();
+
+                l_tmp.cle = tmp[0].Replace(" ","").Replace("\t","") ;
+                l_tmp.valeur = tmp[1].Replace(" ", "");
+                l_lang.trads.Add(l_tmp);
             }
         }
 
-        return result;
+        return l_lang;
     }
+
+    void Start()
+    {
+        langues = new List<Langage>();
+        langues.Add(readFile("francais"));
+    }
+
 }
+[System.Serializable]
+public struct Langage
+{
+    public string langue;
+    public List<Traduction> trads;
+}
+
+[System.Serializable]
+public struct Traduction
+{
+    public string cle;
+    public string valeur;
+}
+
---
+++ Assets/MetaBot/Game/WarBot/Interpreter/Traducteur.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Traducteur : MonoBehaviour {
+    public string langue;
+    public string textOriginal;
+    public string traduction;
+
+    public void Traduction()
+    {
+        LangageLoader ll = GameObject.Find("GameManager").GetComponent<LangageLoader>();
+        foreach(Langage l in ll.langues)
+        {
+            if (l.langue == langue)
+            {
+                foreach(Traduction t in l.trads)
+                {
+                    if (textOriginal == t.cle)
+                    {
+                        traduction = t.valeur;
+                        return;
+                    }
+                }
+            }
+        }
+
+        traduction = textOriginal;
+    }
+	// Use this for initialization
+	void Start () {
+        Traduction();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
