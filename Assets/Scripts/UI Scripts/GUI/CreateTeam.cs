d34cf5e7fcfbcadfeb0db16e1de1eafc412d7029
Daydoosh <fumey.david@hotmail.com>
2018-04-10 15:46:11 +0200

    #18 Ajout bouton "créer équipe"
    Début de mise en place du bouton "créer nouvelle équipe"

---
+++ Assets/Scripts/UI Scripts/GUI/CreateTeam.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateTeam : MonoBehaviour
+{
+    public GameObject window;
+
+
+    public void NameInput()
+    {
+        window.SetActive(true);
+    }
+
+    public void disableWindow()
+    {
+        window.SetActive(false);
+    }
+
+    public void validateName()
+    {
+        window.SetActive(false);
+
+    }
+}
+
+
