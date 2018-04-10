51ad2f3680021cde0b7491f543e23f23d7a9914c
Mehdi Benlamine <mehdi.ben-lamine@etu.unice.fr>
2018-04-10 19:47:32 +0200

    Correction de bug

--- Assets/MetaBot/Game/WarBot/Script/Unit/Other/MovableCharacter.cs
+++ Assets/MetaBot/Game/WarBot/Script/Unit/Other/MovableCharacter.cs
@@ -14,6 +14,8 @@ public class MovableCharacter : MonoBehaviour
 
     public bool a;
     public bool b;
+    public bool c;
+    public bool d;
     public GameObject B;
 
     private Vector3 nextposition;
@@ -24,7 +26,7 @@ public class MovableCharacter : MonoBehaviour
         vectMov = Utility.vectorFromAngle(GetComponent<Stats>()._heading) ;
         nextposition = transform.position + vectMov.normalized * speed * Time.deltaTime;
         
-        if (!_isblocked) { transform.position = nextposition; }
+        transform.position = nextposition;
         _isblocked = isBlocked();
         _obstacleEncounter = false;
     }
@@ -35,14 +37,22 @@ public class MovableCharacter : MonoBehaviour
         vectMov = Utility.vectorFromAngle(GetComponent<Stats>()._heading);
         nextposition = transform.position + vectMov.normalized * speed * Time.deltaTime;
         Ray rA = new Ray(nextposition + vectMov.normalized * _offsetGround, Vector3.down * _edgeDistance);
-        Ray rB = new Ray(nextposition + vectMov.normalized * _offsetObstacle, transform.right* 0.25f);
+        Ray rB = new Ray(nextposition + vectMov.normalized * _offsetObstacle, transform.right);
+        Ray rC = new Ray(nextposition + vectMov.normalized * _offsetObstacle, Vector3.RotateTowards(transform.right, Vector3.right, Mathf.PI / 2, 0.0f));
+        Ray rD = new Ray(nextposition + vectMov.normalized * _offsetObstacle, Vector3.RotateTowards(transform.right, Vector3.left, Mathf.PI/2, 0.0f));
+
 
         RaycastHit hit;
+        RaycastHit hitL;
+        RaycastHit hitR;
         a = Physics.Raycast(rA.origin,rA.direction, rA.direction.magnitude);
         b = Physics.Raycast(rB, out hit, rB.direction.magnitude);
+        c = Physics.Raycast(rC, out hitR, rC.direction.magnitude);
+        d = Physics.Raycast(rD, out hitL, rD.direction.magnitude);
         Debug.DrawRay(rB.origin, rB.direction, Color.black);
         Debug.DrawRay(rA.origin, rA.direction, Color.red);
-
+        Debug.DrawRay(rC.origin, rC.direction, Color.grey);
+        Debug.DrawRay(rD.origin, rD.direction, Color.cyan);
 
         B = null;
         if (b)
@@ -55,6 +65,28 @@ public class MovableCharacter : MonoBehaviour
                 return true;
             }
             else { Debug.DrawRay(rB.origin, rB.direction, Color.white);  }
+        }
+        if (c)
+        {
+
+            if (hitR.transform.tag == "Unit" && hitR.transform.gameObject != gameObject && !hitR.collider.isTrigger)
+            {
+                Debug.DrawRay(rC.origin, rC.direction, Color.red);
+                B = hitR.transform.gameObject;
+                return true;
+            }
+            else { Debug.DrawRay(rC.origin, rC.direction, Color.white); }
+        }
+        if (d)
+        {
+
+            if (hitL.transform.tag == "Unit" && hitL.transform.gameObject != gameObject && !hitL.collider.isTrigger)
+            {
+                Debug.DrawRay(rD.origin, rD.direction, Color.red);
+                B = hitL.transform.gameObject;
+                return true;
+            }
+            else { Debug.DrawRay(rD.origin, rD.direction, Color.white); }
         }
         if (!a) { return true; }
         
