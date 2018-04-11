ef56e26813c90b7f242652447f32b893334ddae3
Daydoosh <fumey.david@hotmail.com>
2018-04-11 20:32:58 +0200

    #32 Boutons choix langue
    Début d'implémentation des boutons pour changer de langue

---
+++ Assets/Scripts/UI Scripts/GUI/ChangeLanguage.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChangeLanguage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangementLangue(string newLangage)
+    {
+        string[] lines = System.IO.File.ReadAllLines("properties.yml");
+        int cpt = 0;
+        foreach (string line in lines)
+        {
+            if (line.Contains("Language"))
+            {
+                string[] tmp = line.Split('=');
+                tmp[1] = newLangage;
+                lines[cpt] = tmp[0] + "=" + tmp[1];
+                break;
+            }
+            cpt++;
+        }
+
+        System.IO.File.WriteAllLines("properties.yml", lines);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
