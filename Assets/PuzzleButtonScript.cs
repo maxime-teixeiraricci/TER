13a26b2c16b4539e5955c3aade8a0f544db91a3c
Fireez <benjaminfireez@yahoo.fr>
2018-04-11 21:17:54 +0200

    Fix Traducteur
    Traduction liste pieces puzzle disponibles , ne pas toucher au IF sinon ca casse tout

--- Assets/PuzzleButtonScript.cs
+++ Assets/PuzzleButtonScript.cs
@@ -8,16 +8,30 @@ public class PuzzleButtonScript : MonoBehaviour
     public Text _text;
     public string label;
     public string value;
+    public string langue;
     public string l;
 	// Use this for initialization
 	void Start () {
-        
+        string[] lines = System.IO.File.ReadAllLines("properties.yml");
+        foreach (string line in lines)
+        {
+            if (line.Contains("Language"))
+            {
+                string[] tmp = line.Split('=');
+                langue = tmp[1];
+                break;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update ()
     {
-        label = value.Replace("PERCEPT_", "").Replace("ACTN_", "").Replace("_", " ");
+        Traducteur l = new Traducteur(langue, value);
+        string traduction = l.Traduction();
+        label = traduction.Replace("PERCEPT_", "").Replace("ACTN_", "").Replace("_", " ");
         _text.text = label;
+        //label = value.Replace("PERCEPT_", "").Replace("ACTN_", "").Replace("_", " ");
+        //_text.text = label;
 	}
 }
