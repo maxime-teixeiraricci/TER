cb254b3a31469fc35752c3b55abc6f738f48b8bf
Daydoosh <fumey.david@hotmail.com>
2018-04-11 21:43:24 +0200

    #18 Bouton Suppriemr Equipe
    Début d'implémentation du bouton "Suppriemr Equipe"

---
+++ Assets/Scripts/UI Scripts/GUI/DeleteTeam.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeleteTeam : MonoBehaviour
+{
+
+    public GameObject Window;
+    public GameObject windowDelete;
+    public Dropdown dropdown;
+    Text windowText;
+    public string initialText = "Voulez vous supprimer ";
+
+
+    public void DisableWindow()
+    {
+        
+        Window.SetActive(false);
+    }
+
+    public void Delete()
+    {
+        Window.SetActive(true);
+        windowText = windowDelete.GetComponentInChildren<Text>();
+        windowText.text = initialText;
+        string nameTeam = dropdown.captionText.text;
+        windowText.text = windowText.text + nameTeam + " ?";
+    }
+}
