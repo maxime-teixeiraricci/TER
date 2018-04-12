822f135e86143ad9b65b102119ef5a788a7fa7c8
Mehdi Benlamine <mehdi.ben-lamine@etu.unice.fr>
2018-04-12 18:40:34 +0200

    Correction de bug permettant a la base d'acceder aux percepts des unitées

--- Assets/MetaBot/Game/WarBot/Script/Unit/Percept/PerceptCommon.cs
+++ Assets/MetaBot/Game/WarBot/Script/Unit/Percept/PerceptCommon.cs
@@ -22,24 +22,7 @@ public class PerceptCommon : Percept
         _percepts["PERCEPT_BAG_EMPTY"] = delegate () { return GetComponent<Inventory>()._actualSize == 0; };
         _percepts["PERCEPT_BAG_10"] = delegate () { return GetComponent<Inventory>()._actualSize >= 10; };
         _percepts["PERCEPT_BAG_25"] = delegate () { return GetComponent<Inventory>()._actualSize >= 25; };
-        _percepts["PERCEPT_CONTRACT"] = delegate () { return GetComponent<Stats>()._contract != null; };
-        _percepts["PERCEPT_FOOD"] = delegate ()
-        {
-            GetComponent<Stats>()._target = null;
-            foreach (GameObject gO in GetComponent<Sight>()._listOfCollision)
-            {
-                if (gO && gO.tag == "Item")
-                {
-                    GetComponent<Stats>()._target = gO;
-                    return true;
-                }
-            }
-            return false;
-        };
-        _percepts["PERCEPT_FOOD_NEAR"] = delegate ()
-        {
-            return (_percepts["PERCEPT_FOOD"]()) && (Vector3.Distance(GetComponent<Stats>()._target.transform.position, transform.position) < 4f);
-        };
+
         _percepts["PERCEPT_ENEMY"] = delegate ()
         {
 
@@ -77,44 +60,6 @@ public class PerceptCommon : Percept
                         GetComponent<Stats>()._heading = getAngle(gO);
                         return true;
                     }
-                }
-            }
-            return false;
-        };
-        _percepts["PERCEPT_BASE_NEAR_ALLY"] = delegate ()
-        {
-            GetComponent<Stats>()._target = null;
-            foreach (GameObject gO in GetComponent<Sight>()._listOfCollision)
-            {
-                if (gO&&gO.GetComponent<Stats>() != null && gO.GetComponent<Stats>()._unitType == "Base" && 
-                Vector3.Distance(transform.position, gO.transform.position) < 7f && gO.GetComponent<Stats>()._teamIndex == GetComponent<Stats>()._teamIndex)
-                {
-                    GetComponent<Stats>()._target = gO;
-                    return true;
-                }
-            }
-            return false;
-        };
-
-
-
-        /** PERCEPT **/
-
-        _percepts["CONTRACT_ELIMINATION"] = delegate () { return GetComponent<Stats>()._contract!= null && GetComponent<Stats>()._contract.type == "Elimination"; };
-        _percepts["CONTRACT_ELIMINATION_TARGET_NEAR"] = delegate () {
-            Brain brain = GetComponent<Brain>();
-            Sight sight = brain.GetComponent<Sight>();
-            List<GameObject> _listOfUnitColl = new List<GameObject>();
-            if (GetComponent<Stats>()._contract == null || GetComponent<Stats>()._contract.type != "Elimination") { return false; }
-            EliminationContract contract = (EliminationContract)GetComponent<Stats>()._contract;
-
-            foreach (GameObject gO in sight._listOfCollision)
-            {
-                if (gO && contract._target == gO)
-                {
-                        GetComponent<Stats>()._target = gO;
-                        GetComponent<Stats>()._heading = getAngle(gO);
-                        return true;
                 }
             }
             return false;
--- Assets/MetaBot/Game/WarBot/Script/Unit/Percept/PerceptUnit.cs
+++ Assets/MetaBot/Game/WarBot/Script/Unit/Percept/PerceptUnit.cs
@@ -16,6 +16,61 @@ public class PerceptUnit : PerceptCommon
         base.InitPercept();
         _percepts["PERCEPT_BLOCKED"] = delegate () { return GetComponent<MovableCharacter>().isBlocked(); };
 
+        _percepts["PERCEPT_BASE_NEAR_ALLY"] = delegate ()
+        {
+            GetComponent<Stats>()._target = null;
+            foreach (GameObject gO in GetComponent<Sight>()._listOfCollision)
+            {
+                if (gO && gO.GetComponent<Stats>() != null && gO.GetComponent<Stats>()._unitType == "Base" &&
+                Vector3.Distance(transform.position, gO.transform.position) < 7f && gO.GetComponent<Stats>()._teamIndex == GetComponent<Stats>()._teamIndex)
+                {
+                    GetComponent<Stats>()._target = gO;
+                    return true;
+                }
+            }
+            return false;
+        };
+        /** PERCEPT **/
+
+        _percepts["CONTRACT_ELIMINATION"] = delegate () { return GetComponent<Stats>()._contract != null && GetComponent<Stats>()._contract.type == "Elimination"; };
+        _percepts["CONTRACT_ELIMINATION_TARGET_NEAR"] = delegate () {
+            Brain brain = GetComponent<Brain>();
+            Sight sight = brain.GetComponent<Sight>();
+            List<GameObject> _listOfUnitColl = new List<GameObject>();
+            if (GetComponent<Stats>()._contract == null || GetComponent<Stats>()._contract.type != "Elimination") { return false; }
+            EliminationContract contract = (EliminationContract)GetComponent<Stats>()._contract;
+
+            foreach (GameObject gO in sight._listOfCollision)
+            {
+                if (gO && contract._target == gO)
+                {
+                    GetComponent<Stats>()._target = gO;
+                    GetComponent<Stats>()._heading = getAngle(gO);
+                    return true;
+                }
+            }
+            return false;
+        };
+
+        _percepts["PERCEPT_FOOD_NEAR"] = delegate ()
+        {
+            return (_percepts["PERCEPT_FOOD"]()) && (Vector3.Distance(GetComponent<Stats>()._target.transform.position, transform.position) < 4f);
+        };
+
+        _percepts["PERCEPT_CONTRACT"] = delegate () { return GetComponent<Stats>()._contract != null; };
+        _percepts["PERCEPT_FOOD"] = delegate ()
+        {
+            GetComponent<Stats>()._target = null;
+            foreach (GameObject gO in GetComponent<Sight>()._listOfCollision)
+            {
+                if (gO && gO.tag == "Item")
+                {
+                    GetComponent<Stats>()._target = gO;
+                    return true;
+                }
+            }
+            return false;
+        };
     }
 
 
