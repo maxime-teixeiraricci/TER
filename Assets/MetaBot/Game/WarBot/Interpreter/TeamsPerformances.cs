a9839224676962d8c40c37d853db3b8a0aa6db39
RFOyashiro <kurt.savio+git@hotmail.fr>
2018-04-15 10:36:21 +0200

    Fonction Ecriture Stats
    WriteStats

--- Assets/MetaBot/Game/WarBot/Interpreter/TeamsPerformances.cs
+++ Assets/MetaBot/Game/WarBot/Interpreter/TeamsPerformances.cs
@@ -11,6 +11,34 @@ public class TeamsPerformance : XmlDocument {
     {
     }
 
+    public void WriteStats (string[] Teams, string Winner)
+    {
+        foreach (string Team in Teams)
+        {
+            int i = 0;
+            string[] Stats = System.IO.File.ReadAllLines(Application.streamingAssetsPath + "/Stats/" + Team + ".stat");
+            foreach (string Stat in Stats) 
+            {
+                string[] DetailStat = Stat.Split('/');
+                if (DetailStat[0] == Teams[0] || DetailStat[0] == Teams[1] || DetailStat[0] == Teams[2] || DetailStat[0] == Teams[3])
+                {
+                    int numVal = Int32.Parse(DetailStat[1]);
+                    numVal++;
+                    DetailStat[1] = numVal.ToString(); //NbMatch
+                    if (Team == Winner)
+                    {
+                        numVal = Int32.Parse(DetailStat[2]);
+                        numVal++;
+                        DetailStat[2] = numVal.ToString(); //NbVictoire
+                    }
+                }
+                Stats[i] = DetailStat[0] + '/' + DetailStat[1] + '/' + DetailStat[2];
+                i++;
+            }
+            System.IO.File.WriteAllLines(Application.streamingAssetsPath + "/Stats/" + Team + ".stat", Stats);
+        }
+    }
+    
     public Dictionary<string, List<int>> getStats(string path)
     {
         // Try to find an already existing file with this team name
