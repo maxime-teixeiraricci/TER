f26ff31a58941efc6ebf83ff20763719d1ded525
Daydoosh <fumey.david@hotmail.com>
2018-04-20 11:20:35 +0200

    #35 Ebauche fenetre parametres - Non fonctionnelle -

---
+++ Assets/Scripts/UI Scripts/GUI/Settings.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour {
+
+    public Slider slider;
+    Text text;
+
+	// Use this for initialization
+	void Start () {
+        text = gameObject.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Value text = " + text);
+        text.text = slider.value.ToString();
+	}
+}
