3753f8c518bb5e48504bae49a9691e2b6c827f2d
CrashBomb <guillaume.deau33660@gmail.com>
2018-04-24 15:33:33 +0200

    Ajout d'un message "MESSAGE_POSITION_RESOURCE"

--- Assets/MetaBot/Engine/Unit/MessageManagerUnit.cs
+++ Assets/MetaBot/Engine/Unit/MessageManagerUnit.cs
@@ -5,5 +5,5 @@ using UnityEngine;
 public class MessageManagerUnit : MessageManager
 {
     
-    public override void Init() { _messageType = new string[] { "MESSAGE_HELP", "MESSAGE_POSITION", "MESSAGE_ATTACK" }; }
+    public override void Init() { _messageType = new string[] { "MESSAGE_HELP", "MESSAGE_POSITION", "MESSAGE_ATTACK", "MESSAGE_POSITION_RESOURCE" }; }
 }
