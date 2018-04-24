7b30230a4036a1a76aadfa0011716727ef6380b4
Mehdi Benlamine <mehdi.ben-lamine@etu.unice.fr>
2018-04-24 10:37:42 +0200

    Animation bullets

--- Assets/MetaBot/Game/WarBot/Item/BulletScript.cs
+++ Assets/MetaBot/Game/WarBot/Item/BulletScript.cs
@@ -9,14 +9,17 @@ public class BulletScript : MonoBehaviour
     public Vector3 _vect;
     public float _speed;
     public int _damage;
+    public GameObject Explosion;
 
-	void FixedUpdate ()
+    void FixedUpdate ()
     {
         transform.position += _vect.normalized * Time.deltaTime * _speed;
         _lifeTime -= Time.deltaTime;
         if (_lifeTime < 0f)
         {
+            GameObject explosion = Instantiate(Explosion, transform.position, Quaternion.identity);
             Destroy(gameObject);
+            Destroy(explosion, 1.5f);
         }
 
     }
@@ -27,9 +30,12 @@ public class BulletScript : MonoBehaviour
         if (!other.isTrigger && other.GetComponent<Stats>() && other.gameObject != _owner.gameObject)
         {
             print(other + " Touched");
+            GameObject explosion = Instantiate(Explosion, transform.position, Quaternion.identity);
             other.GetComponent<Stats>()._health -= _damage;
             _damage = 0;
-            Destroy(gameObject);
+
+            Destroy(gameObject, 0.25f);
+            Destroy(explosion,1.5f);
         }
     }
 }
