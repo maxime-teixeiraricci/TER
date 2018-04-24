ecc9d3012ff7ad72fe2d154f03f74f1fe9efa11f
Maxime Teixeira <maxime.teixeira.93@gmail.com>
2018-04-24 11:57:27 +0200

    Ajout de la vision des unités + Correction de bugs liés aux unités

--- Assets/MetaBot/Game/WarBot/Script/Unit/Skill/Shooter.cs
+++ Assets/MetaBot/Game/WarBot/Script/Unit/Skill/Shooter.cs
@@ -7,15 +7,18 @@ public class Shooter : MonoBehaviour
     public Transform[] _listOfStartPoint;
     public GameObject _weapon;
     private int _i;
+    public int reloadTick = 0;
 	
     public void Shoot()
     {
-        print("Fire !!!");
-        GameObject weapon = Instantiate(_weapon);
-        _i = (_i + 1) % _listOfStartPoint.Length;
-        weapon.transform.position = _listOfStartPoint[_i].position;
-        weapon.GetComponent<BulletScript>()._owner = gameObject;
-        weapon.GetComponent<BulletScript>()._vect = Utility.vectorFromAngle(GetComponent<Stats>()._heading);
-        GetComponent<Stats>()._reloadTime = 2.5f;
+        if (reloadTick == 0)
+        {
+            GameObject weapon = Instantiate(_weapon);
+            _i = (_i + 1) % _listOfStartPoint.Length;
+            weapon.transform.position = _listOfStartPoint[_i].position;
+            weapon.GetComponent<BulletScript>()._owner = gameObject;
+            weapon.GetComponent<BulletScript>()._vect = Utility.vectorFromAngle(GetComponent<Stats>()._heading);
+            reloadTick = GetComponent<Stats>()._reloadTime;
+        }
     }
 }
