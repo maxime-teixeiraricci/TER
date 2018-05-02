af49dd877c4d36ffc5cf79abf8b74e8e570caae1
Daydoosh <fumey.david@hotmail.com>
2018-05-02 19:55:11 +0200

    #35 Changement visuel bouton Paramètres

--- Assets/Scripts/UI Scripts/GUI/SettingsButton.cs
+++ Assets/Scripts/UI Scripts/GUI/SettingsButton.cs
@@ -9,6 +9,7 @@ public class SettingsButton : MonoBehaviour {
     Button editButton;
     Button quitButton;
     Button reloadButton;
+    Button settingsButton;
 
     AudioSource gm;
 
@@ -114,10 +115,12 @@ public class SettingsButton : MonoBehaviour {
         editButton = GameObject.Find("EditButton").GetComponent<Button>();
         quitButton = GameObject.Find("QuitButton").GetComponent<Button>();
         reloadButton = GameObject.Find("ReloadButton").GetComponent<Button>();
+        settingsButton = GameObject.Find("SettingsButton").GetComponent<Button>();
         playButton.interactable = true;
         editButton.interactable = true;
         quitButton.interactable = true;
         reloadButton.interactable = true;
+        settingsButton.interactable = true;
     }
 
     void setButtonInactive()
@@ -126,9 +129,11 @@ public class SettingsButton : MonoBehaviour {
         editButton = GameObject.Find("EditButton").GetComponent<Button>();
         quitButton = GameObject.Find("QuitButton").GetComponent<Button>();
         reloadButton = GameObject.Find("ReloadButton").GetComponent<Button>();
+        settingsButton = GameObject.Find("SettingsButton").GetComponent<Button>();
         playButton.interactable = false;
         editButton.interactable = false;
         quitButton.interactable = false;
         reloadButton.interactable = false;
+        settingsButton.interactable = false;
     }
 }
